98636b737f8086ef892e95f2e7453e57a6eca0ec
acemavrick <116685696+acemavrick@users.noreply.github.com>
2025-05-15 21:54:05 -0500

    Initial Commit

---
+++ Mazes/MazesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazesApp.swift
+//  Mazes
+//
+//  Created by acemavrick on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MazesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
